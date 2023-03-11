0e215277e807c8b08220dfad4439a5140f1380cc
Natalya <nataliettaa@mail.ru>
2023-03-11 22:05:59 +0500

    task solved

---
+++ klasswork/task8.py
@@ -0,0 +1,16 @@
+# Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если
+# разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
+# *Пример:*
+# 3 2 4 -> yes
+# 3 2 1 -> no
+
+number_n = int(input("Введите размер n: "))
+number_m = int(input("Введите размер m: "))
+number_k = int(input("Введите количество долек, которые необходимо отломить (k): "))
+
+if number_k % number_n == 0 and number_k < number_n*number_m:
+    print("yes")
+elif number_k % number_n == 0 and number_k < number_n*number_m:
+    print("yes")
+else:
+    print("no")
