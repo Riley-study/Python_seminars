f85ad0d1d312dbc3f5cf38cbf3fd6c5852bb8a27
Natalya <nataliettaa@mail.ru>
2023-03-11 20:14:51 +0500

    task solved

--- homework1/Task2.py
+++ homework1/Task2.py
@@ -2,3 +2,11 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
+
+number = int(input("Введите трехзначное число: "))
+first_number = number%10
+second_number = number//10%10
+third_number = number//100%10
+print(f'сумма цифр равна {first_number+second_number+third_number}')
+
+
