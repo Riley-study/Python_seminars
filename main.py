c402f900b376bbd1db2268d9011d8dcfc1d1e585
Natalya <nataliettaa@mail.ru>
2023-03-11 19:28:10 +0500

    classwork 1 проба пера

---
+++ main.py
@@ -0,0 +1,13 @@
+
+first_number_vagon = int(input("Введите вагон, в который зашел Витя: "))
+second_number_vagon= int(input("Введите номер вагона, в который попал Витя: "))
+
+if (first_number_vagon == second_number_vagon):
+    print("недостаточно условий для вычисления")
+else:
+    numers_vagon = first_number_vagon+second_number_vagon-1
+    print(f'Витя сел в поезд, в котором {numers_vagon} вагонов')
+
+# desks = first_class//2+bool(first_class%2!=0)+second_class//2+bool(second_class%2!=0)+therd_class//2+bool(therd_class%2!=0)
+# print(f'Всего понадобится {desks} парт')
+
