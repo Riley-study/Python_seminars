f49f80d2267a016c0eb7cc9f96d243c121d0116b
Natalya <nataliettaa@mail.ru>
2023-03-15 12:31:52 +0500

    task about summ and product numbers solved

--- homework2/task12.py
+++ homework2/task12.py
@@ -7,13 +7,13 @@ product_number = int(input("введите произведение чисел: 
 
 first_number = 0
 second_number = 0
-while first_number <= summ_number:
+while first_number < summ_number:
     second_number = summ_number - first_number
     if first_number*second_number == product_number:
         print(f'первое число {first_number}, второе число {second_number}')
-        first_number += 1
-    else:
-        first_number += 1
-if first_number > summ_number:
-    print("таких чисел не существует")
+    first_number += 1
+
+else:
+    if first_number > summ_number:
+        print("таких чисел не существует")
 
