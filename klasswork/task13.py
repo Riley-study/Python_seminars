d804bfa8ec84c217849e0e6e6ad7bfe621d25b92
Natalya <nataliettaa@mail.ru>
2023-03-29 09:54:40 +0500

    task 12 solved)

--- klasswork/task13.py
+++ klasswork/task13.py
@@ -4,7 +4,7 @@
 # превосходящее 105. Программа должна вывести все пары дружественных чисел, каждое из которых не превосходит k. Пары
 # необходимо выводить по одной в строке, разделяя пробелами. Каждая пара должна быть выведена только один раз
 # (перестановка чисел новую пару не дает).
-
+import time
 
 numb_k = 3000
 list_delit = []
@@ -14,11 +14,19 @@ for i in range(1, numb_k):
         if i % item == 0:
             count += item
     list_delit.append(count)
-print(list_delit)
+# print(list_delit)
 # print(list_delit[219], list_delit[283])
+
+dict_friends_number = {}
 
 for item in range(1, numb_k-1):
     for i in range(1, numb_k-1):
         if list_delit[item-1] == i and list_delit[i-1] == item and i != item:
-            print(list_delit[item-1], list_delit[i-1])
+            dict_friends_number[item] = dict_friends_number.get(item, i)
+            # print(f'дружественная пара чисел {list_delit[item-1]}, {list_delit[i-1]}')
 
+list_pair = []
+for key, value in dict_friends_number.items():
+    if (value, key) not in list_pair:
+        list_pair.append((key, value))
+        print(key, value)
