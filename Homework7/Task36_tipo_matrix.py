7478f90a027e1cb3df658c19a5bb7989485de5f4
Natalya <nataliettaa@mail.ru>
2023-04-02 18:16:01 +0500

    task 34 solved

--- Homework7/Task36_tipo_matrix.py
+++ Homework7/Task36_tipo_matrix.py
@@ -12,3 +12,31 @@
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+num_rows, num_columns = 6, 6
+
+
+def print_operation_table(rows, columns):
+    for i in range(1, rows+1):
+        res_list = list()
+        for j in range(1, columns + 1):
+            res_list.append(i * j)
+        print(*res_list)
+
+
+print_operation_table(num_rows, num_columns)
+
+
+def operation(x, y):
+    list_a = [i for i in range(1, x+1)]
+    for item in range(1, y+1):
+        list_b = list(map(lambda c: c * item, list_a))
+        print(*list_b)
+
+operation(num_rows, num_columns)
+
+# >>> a = [1, 2, 3, 4, 5, 6, 7]
+# >>> b = list(map(lambda x: x**2, a))
+# >>> print('a = {}\nb = {}'.format(a, b))
+# a = [1, 2, 3, 4, 5, 6, 7]
+# b = [1, 4, 9, 16, 25, 36, 49]
